e7f09a2a0efc1233b92172d1ee0c363993bbe1f0
davidmaniibrahim <ibrahimmanidavid@gmail.com>
2025-09-24 19:21:11 +0100

    remove space in admin container

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -11,12 +11,12 @@ const AdminPage = () => {
         <AppSidebar />
 
         {/* Main Content */}
-        <div className="flex flex-col flex-1 overflow-hidden">
+        <div className="">
           {/* Header */}
           {/* <Header /> */}
 
           {/* Main Content Area */}
-          <main className="flex-1 overflow-y-auto p-4">
+          <main className="">
             <Outlet />
           </main>
         </div>
